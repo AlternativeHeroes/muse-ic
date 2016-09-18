dca00e441b9705194a4b30c50bce7c162d390da0
Andrew Dai <theman@andrewdai.co>
2016-09-18 02:24:45 -0400

    unbucket

--- gen_fft.py
+++ gen_fft.py
@@ -8,7 +8,7 @@ Take some wav files, get the spectrum
 from __future__ import division
 import sys
 # import matplotlib.pyplot as plt
-from scipy.fftpack import fft
+from scipy.fftpack import fft, ifft
 from scipy.fftpack import fftfreq
 from scipy.io import wavfile
 import pandas
@@ -41,15 +41,14 @@ def analyze(music, bitdepth=None, bitrate=44100, sample_time=1/6.0):
     print("Normalizing music to standard volume")
     normed = mono / (1 << (bitdepth - 1))
 
-    n = len(normed)
-    music_time = n / bitrate
+    music_time = len(normed) / bitrate
 
     samples = []
 
+    n = int(sample_time * bitrate)
     for sample in range(0, int(np.ceil(music_time / sample_time))):
-        start = int(sample * sample_time * bitrate)
-        end = int(start + sample_time * bitrate)
-        n = end - start
+        start = int(sample * n)
+        end = int(start + n)
 
         # print("Analyzing [%d, %d)" % (start, end), file=sys.stderr)
         print "Analyzing [%d, %d)" % (start, end)
@@ -115,7 +114,7 @@ def make_sin():
 def f(n):
     return 440 * 2 ** ((float(n)-49) / 12)
 
-ranges = [0] + [f(n + 0.5) for n in range(0,89)]
+ranges = [f(n) for n in xrange(0,89)]
 log2 = np.log(2)
 thirteenlog2 = 13 * log2
 
@@ -128,23 +127,61 @@ def finv(freq):
 '''
     sample is an array of (freq, amp)
 '''
-def processSample(sample, id=None):
+def bucketSample(sample, id=None):
     processed = np.zeros(90)
     for freq, amp in sample:
         key = min(max(int(np.round((finv(freq)))), 0), 89)
         processed[key] = processed[key] + amp
-    print "Done with sample", id
+    if id is not None:
+        print "Done with sample", id
     return processed
+
+'''
+    bitrate is the rate for the output wave file
+'''
+def unbucketSample(processed, keymap, nfreqs=3674, id=None):
+    unbucket = np.zeros(nfreqs)
+    for key, amp in enumerate(processed):
+        unbucket[keymap[key]] = amp
+    return unbucket
 
 
 if __name__ == "__main__":
     import md5
-    sf, music = load_wav(sys.argv[1])
-    music = middle_30(music, sf)
-    analysis = analyze(music, bitrate=sf)
-    processed = [processSample(sample, id) for id, sample in enumerate(analysis)]
-    dataFrame = pandas.DataFrame(processed)
+    sample_time = 1/6.0
+
+    bitrate, music = load_wav(sys.argv[1])
+    music = middle_30(music, bitrate)
+
+    n = int(bitrate * sample_time)
+    freqs = fftfreq(n, d=(1 / bitrate))[1:int(n/2)]
+    print len(freqs)
+
+    analysis = analyze(music, bitrate=bitrate, sample_time=sample_time)
+
+    bucketed = [bucketSample(sample, id) for id, sample in enumerate(analysis)]
+
     hash_file = md5.new()
     hash_file.update(sys.argv[1])
-    file_name = hash_file.digest().encode('base64').replace("/", "").replace("=", "").strip() + ".csv"
-    dataFrame.to_csv(file_name, index=False, index_label=False, header=False)
+    file_name = hash_file.digest().encode('base64').replace("/", "").replace("=", "").strip()
+    inputData = pandas.DataFrame(bucketed)
+    inputData.to_csv(file_name + ".csv", index=False, index_label=False, header=False)
+
+    keys = [x for x in xrange(0, 90)]
+    keymap = {}
+    for key in keys:
+        index = None
+        keyfreq = f(key)
+        for i, freq in enumerate(freqs):
+            if freq > keyfreq:
+                index = i
+                break
+        if index is None:
+            print 'NOOOOOOOOO'
+            exit(100)
+        keymap[key] = index
+
+    print keymap
+    unbucketed = [unbucketSample(sample, keymap, nfreqs=len(freqs), id=id) for sample in bucketed]
+    outputData = pandas.DataFrame(unbucketed)
+    # reproducedSong = reproduceSong(unbucketed)
